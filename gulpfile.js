9f6fb16f265e50dc61065f1f5f8a0e6d28c7fc65
Ksenia <ksenija.kopteva@gmail.com>
2019-09-06 17:14:49 +0300

    Changed gulp structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,59 @@
-const {
-  watch,
-  series,
-  parallel,
-  src,
-  dest
-} = require("gulp");
+const { watch, series, parallel, src, dest } = require("gulp");
 const autoprefixer = require("autoprefixer");
 const postcss = require("gulp-postcss");
 const sass = require("gulp-sass");
-const browserSync = require('browser-sync').create();
+const browserSync = require("browser-sync").create();
 
 function styles() {
-  return src('sass/styles.scss', {
-      sourcemaps: true
-    })
+  return src("sass/styles.scss", {
+    sourcemaps: true
+  })
     .pipe(
       sass({
         outputStyle: "expanded",
         indentType: "space",
-        indentWidth: "4",
+        indentWidth: "4"
       }).on("error", sass.logError)
     )
     .pipe(postcss([autoprefixer("last 2 versions", "> 1%")]))
-    .pipe(dest('./', {
-      sourcemaps: true
-    }))
+    .pipe(
+      dest("./dist", {
+        sourcemaps: true
+      })
+    );
+}
+
+function build() {
+  return src("sass/styles.scss", {
+    sourcemaps: false
+  })
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+        indentType: "space",
+        indentWidth: "4"
+      }).on("error", sass.logError)
+    )
+    .pipe(postcss([autoprefixer("last 2 versions", "> 1%")]))
+    .pipe(
+      dest("./dist", {
+        sourcemaps: false
+      })
+    );
 }
 
 function server() {
   browserSync.init({
     notify: false,
     server: {
-      baseDir: './'
+      baseDir: "./dist"
     }
   });
 }
 
 //watch(sassWatch, styles)
-watch('sass/**/*', styles);
-watch('./**/*').on('change', browserSync.reload);
+watch("sass/**/*", styles);
+watch("./**/*").on("change", browserSync.reload);
 
-exports.default = series(styles, server)
+exports.build = build;
+exports.default = series(styles, server);
